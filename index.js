27adb6514a39b8fa2f24efd849ea8ee69a82a4b1
Nattawat Imthet <60160039@go.buu.ac.th>
2020-11-07 18:25:15 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const item = [
+    { id: 1, name:'iphone 12'},
+    { id: 2, name:'Nokia 3310'}
+]
+
+app.get('/', (req, res) => {
+    return res.send('Hello!')
+})
+
+app.listen(port, () => {
+    console.log(`Start Express application at ${port}`)
+})
+
+module.exports = app
